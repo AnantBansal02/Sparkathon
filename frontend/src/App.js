a74dbb480c29c60c72aaeddf49d7fe1178360097
Vatsal Srivastava <202151176@iiitvadodara.ac.in>
2024-08-15 17:25:58 +0530

    login-signup updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import Signup from './Pages/SignUp'
-import Navbar from './Components/NavBar'
-import Login from './Pages/Login'
+import { useState } from "react";
+import Signup from "./Pages/SignUp";
+import Navbar from "./Components/NavBar";
+import Login from "./Pages/Login";
+import SideDesign from "./Components/SideDesign";
 
 function App() {
   return (
     <>
-      <Navbar />
-    <div className='p-4 h-screen flex items-center justify-center'>
-      <Signup />
-      {/* <Login /> */}
-    </div>
+      {/* <Navbar /> */}
+      <div className="h-screen flex items-center justify-center">
+        <SideDesign />
+        {/* <Signup /> */}
+        {/* <Login /> */}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
